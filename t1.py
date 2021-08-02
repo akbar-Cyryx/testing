0cae4a7a80deb668cfad8f25428e5215c6c5441d
akbar-Cyryx <69419222+akbar-Cyryx@users.noreply.github.com>
2021-08-02 22:26:33 +0500

    Initial Commit

---
+++ t1.py
@@ -0,0 +1,7 @@
+listofNumber = [1,2,3,4,5,6,7,8,9,10]
+for number in listofNumber:
+    if(number%2 == 0):
+        print(number, " is Even")
+    else:
+        print(number, " is Odd")
+print("All Done")
